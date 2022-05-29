08f755f5094ef12aeaf8e1ff34458ff6a15d700e
astakh <v.astakhov@gmail.com>
2022-05-29 07:38:33 +0300

    first3

--- admin.js
+++ admin.js
@@ -2,17 +2,18 @@ import fetch from 'node-fetch';
 import {State}    from './models/state.js';
 import {nowTime} from './functions.js'
 import {stateIs, lastAction, stateOn, stateOff} from './func_DB.js'
+import 'dotenv/config';
 
 //const Log = require('./models/log.js');
 import mongoose from 'mongoose';
 
-const db = 'mongodb+srv://admin:mongoDedperded999@cluster0.iq3e4.mongodb.net/WavesBot?retryWrites=true&w=majority';
+const db = process.env.DBPATH;
 mongoose
     .connect(db)
     .then((res) => console.log('Connected to DB'))
     .catch((err) => console.log(err));
 
-global.stateDB = '6290c6a80914904a10cbb78f';
+global.stateDB = process.env.PROCSTATE;
 
 stateIs();
 stateOn();
